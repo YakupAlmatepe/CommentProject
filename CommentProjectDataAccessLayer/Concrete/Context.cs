89b20d8cdff2f2e13f4a4f100f82d583ab228263
YakupAlmatepe <almatepe.yakup@gmail.com>
2023-04-09 06:19:42 +0300

    Add project files.

---
+++ CommentProjectDataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using CommentProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommentProjectDataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-L7LL5EMD;initial catalog =DbComment; integrated security=true;");
+        }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
